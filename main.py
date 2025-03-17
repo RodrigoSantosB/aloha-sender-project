351ebeb68f10fe2d284381d412034016ffcdb900
paivalucass <lucaspaiva2805@gmail.com>
2025-03-17 10:57:17 -0300

    feat: add sender codes

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-import keyboard
+# import keyboard
 from scripts.talk import talk
 
 if __name__ == "__main__":
     while True:
         # Wait for the space key to be pressed
-        print("Pressione ESPAÇO para falar...")
-        keyboard.wait("space")  # Blocks execution until space is pressed
+        # print("Pressione ESPAÇO para falar...")
+        # keyboard.wait("space")  # Blocks execution until space is pressed
         
         text = talk()  # Call the talk function when space is pressed
