c24a71ad5c4a879af7375797fdb8e3e4159b5baf
Rodrigo <rsb6@cin.ufpe.br>
2025-03-29 11:30:23 -0300

    update lora module

---
+++ ino/lora_base/lora_tx/lora_tx.ino
@@ -0,0 +1,23 @@
+#include <SPI.h>
+#include <LoRa.h>
+
+int counter = 0;
+
+void setup() {
+  Serial.begin(9600);
+
+  while (!Serial);
+  Serial.println("Transmissor LoRa");
+  if (!LoRa.begin(433E6)) {           // Frequencia de operação (ou 915E6)
+    Serial.println("Falha em iniciar o LoRa!");
+    while (1);
+  }
+}
+
+void loop() {
+  counter++;
+  LoRa.beginPacket();
+  LoRa.print(counter);
+  LoRa.endPacket();
+  delay(1000);
+}
